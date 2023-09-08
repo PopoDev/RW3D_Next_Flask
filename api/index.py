f720b5e910f5975252b20d22892d6f278a685003
PopoDev <popocompte@gmail.com>
2023-09-08 16:45:43 +0200

    fix: error serverless function exceed max size V4

--- api/index.py
+++ api/index.py
@@ -4,14 +4,6 @@ import plotly.express as px
 
 app = Flask(__name__)
 
-@app.route('/api')
-def home():
-    return "<h1>Flask Backend API</h1>"
-
-@app.route('/api/python')
-def hello_world():
-    return "<p>Hello world!</p>"
-
 @app.route('/api/sim', methods=['POST'])
 def api_sim():
     data = request.json
@@ -24,31 +16,6 @@ def api_sim():
 
     sim = Simulation(n, dim=dim, init=init, v=v)
     df = sim.animate(steps=steps)
-    fig = px.scatter_3d(df, x="x", y="y", z="z", animation_frame="step", range_x=[0, dim[0]], range_y=[0, dim[1]], range_z=[0, dim[2]])
-    fig.update_layout(
-        title="Random Walk Simulation",
-        title_x=0.5,
-        scene={
-            'xaxis': {'range': [0, dim[0]]},
-            'yaxis': {'range': [0, dim[1]]},
-            'zaxis': {'range': [0, dim[2]]},
-            'aspectmode': 'cube',
-        },
-    )
-    
-    fig.show()
-
-    return [{
-        "x": df_step["x"].to_json(orient="records"),
-        "y": df_step["y"].to_json(orient="records"),
-        "z": df_step["z"].to_json(orient="records")
-        } for step, df_step in df.groupby('step')]
-
-@app.route('/test/sim')
-def sim():
-    dim = (100, 100, 100)
-    sim = Simulation(n=10000, dim=dim, init="center")
-    df = sim.animate(steps=10)
     fig = px.scatter_3d(df, x="x", y="y", z="z", animation_frame="step", range_x=[0, dim[0]], range_y=[0, dim[1]], range_z=[0, dim[2]])
     fig.update_layout(
         title="Random Walk Simulation",
