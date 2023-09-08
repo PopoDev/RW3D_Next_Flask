996d45fb1700e52728a6efe846179583112ed9bc
PopoDev <popocompte@gmail.com>
2023-09-08 15:39:35 +0200

    fix: install using pnpm

--- app/simulation/page.tsx
+++ app/simulation/page.tsx
@@ -204,16 +204,20 @@ export default function SimulationPage() {
             </FormControl>
           </Grid>
           <Grid item xs={12}>
-            <button
+            <Button
               type="submit"
-              className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br 
-              focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 
-              font-medium rounded-lg text-sm p-4 my-12 text-center w-[270px]"
+              variant="contained"
+              style={{
+                marginTop: "20px",
+                padding: "16px",
+                width: "270px",
+                backgroundColor: "rgb(34 197 94)",
+              }}
             >
               <p className="text-2xl font-bold font-mono text-black">
                 Start Simulation
               </p>
-            </button>
+            </Button>
           </Grid>
         </Grid>
       </form>
