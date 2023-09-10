9b68d76b59318ccc477213ec8b0eb1d81075a26f
PopoDev <popocompte@gmail.com>
2023-09-10 12:37:33 +0200

    feat: deploy on two projects to handle Vercel's Serverless Function max size

--- api/views.py
+++ api/views.py
@@ -1,7 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from src.simulation import Simulation
 
 views = Blueprint('views', __name__)
 
-@views.route('/python')
-def python():
-    return '<h1>Flask Backend</h1>'
+@views.route('/sim', methods=['POST'])
+def api_sim():
+    data = request.json
+    n = int(data.get('number_particles'))
+    steps = int(data.get('steps'))
+    dim = (int(data.get('dim')[0]), int(data.get('dim')[1]), int(data.get('dim')[2]))
+    v = (float(data.get('v')[0]), float(data.get('v')[1]), float(data.get('v')[2]))
+    init = data.get('init')
+
+    sim = Simulation(n, dim=dim, init=init, v=v)
+    df = sim.animate(steps=steps)
+
+    return [{
+        "x": df_step["x"].to_json(orient="records"),
+        "y": df_step["y"].to_json(orient="records"),
+        "z": df_step["z"].to_json(orient="records")
+        } for step, df_step in df.groupby('step')]
